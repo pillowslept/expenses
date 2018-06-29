d58874d1c61c36909e9c77edbd0498b6cb47a50d
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-06-28 21:56:37 -0500

    Component created for manipulate and generate charts.

---
+++ src/main/java/co/com/expenses/component/Charts.java
@@ -0,0 +1,52 @@
+package co.com.expenses.component;
+
+import static java.awt.Color.WHITE;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.PieChart;
+import org.knowm.xchart.PieChartBuilder;
+import org.knowm.xchart.BitmapEncoder.BitmapFormat;
+import org.springframework.stereotype.Component;
+
+import co.com.expenses.dto.ChartSeries;
+
+@Component
+public class Charts {
+
+    private static final int HEIGHT = 250;
+    private static final int WIDTH = 350;
+
+    private static final String ERROR_OBTAINING_BYTES = "Ocurrió un error obteniendo los bytes para la gráfica";
+    private static final Logger LOGGER = Logger.getLogger(PdfReport.class.getName());
+
+    public PieChart pie(List<ChartSeries> listChartSeries) {
+
+        PieChart chart = new PieChartBuilder().width(WIDTH).height(HEIGHT).build();
+
+        listChartSeries.stream().forEach(ch -> chart.addSeries(ch.getName(), ch.getValue()));
+
+        chart.getStyler().setChartBackgroundColor(WHITE);
+        chart.getStyler().setLegendBorderColor(WHITE);
+        chart.getStyler().setPlotBorderColor(WHITE);
+
+        return chart;
+    }
+
+    public byte[] bytes(PieChart chart) {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = BitmapEncoder.getBitmapBytes(chart, BitmapFormat.PNG);
+        } catch (IOException e) {
+            LOGGER.error(ERROR_OBTAINING_BYTES, e);
+        }
+        return bytes;
+    }
+
+    public ChartSeries buildSerie(String name, Number value) {
+        return ChartSeries.builder().value(value).name(name).build();
+    }
+}
