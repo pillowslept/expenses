bbeb167bd19f80d6bfb8562365527380189d2b6a
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2019-01-21 22:30:43 -0500

    Utilities deleted from project.

--- src/main/java/co/com/expenses/util/Constants.java
+++ src/main/java/co/com/expenses/util/Constants.java
@@ -2,11 +2,7 @@ package co.com.expenses.util;
 
 public class Constants {
 
-    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
-    public static final String PATH_SEPARATOR = System.getProperty("path.separator");
     public static final String PDF_FILE_EXTENSION = "pdf";
-
-    public static final String SPACE = " ";
 
     private Constants() {
     }
